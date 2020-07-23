760c0fc6fd586297ad5b6c1d5504c9c365bacaf1
edupanov <edupanov@gmail.com>
2020-07-23 17:02:17 +0300

    add deploy

---
+++ build/precache-manifest.0f27b0a109b959d3e8b7df7d836c0383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745e38b624a3fe1c70fa7e0084b8f944",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "5722b8aefbea27747c49",
+    "url": "/homework/static/css/main.9597b87c.chunk.css"
+  },
+  {
+    "revision": "55d53212b06022058f10",
+    "url": "/homework/static/js/2.ffdebb73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.ffdebb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5722b8aefbea27747c49",
+    "url": "/homework/static/js/main.ff23d9e0.chunk.js"
+  },
+  {
+    "revision": "a4e48f1684aa8f502b8e",
+    "url": "/homework/static/js/runtime-main.d656ed70.js"
+  }
+]);
