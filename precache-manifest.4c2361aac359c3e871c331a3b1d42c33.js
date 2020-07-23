891a762f7880e8ae179160a9b968105553f69ee3
edupanov <edupanov@gmail.com>
2020-07-23 17:09:42 +0300

    Updates

---
+++ precache-manifest.4c2361aac359c3e871c331a3b1d42c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab6809742e7fa82fc7f3c4e9ebe5f03",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "2dc757bf50c71c20d94c",
+    "url": "/homework/static/css/main.67c7be77.chunk.css"
+  },
+  {
+    "revision": "55d53212b06022058f10",
+    "url": "/homework/static/js/2.ffdebb73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.ffdebb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc757bf50c71c20d94c",
+    "url": "/homework/static/js/main.6d5e1156.chunk.js"
+  },
+  {
+    "revision": "a4e48f1684aa8f502b8e",
+    "url": "/homework/static/js/runtime-main.d656ed70.js"
+  }
+]);
